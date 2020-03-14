ef0c44bfe11dff77c5e76630339ca20576dfa8b3
ankushpatiltcl22 <61946519+ankushpatiltcl22@users.noreply.github.com>
2020-03-15 00:10:23 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions calculate the inverse of a matrix and saves it
+## This function creates a special "matrix" object 
+## Stage1:- Initially set the value of the matrix
+## Stage2:- get the value of the matrix
+## Stage3:- set the value of the inverse
+## Stage4:- get the value of the inverse
 
-## Write a short comment describing this function
+## Mainly used variables in the functions are matrix,x,y
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## define the cache matrix
+        matrix <- NULL
+        set <- function(y) {
+                x <<- y ## assign the input matrix y to the variable x in the environment
 
+                matrix <<- NULL ## re-initialize matrix in the environment to null
+        }
+        get <- function() x ## return the matrix x
+        setinverse <- function(inverse) matrix <<- inverse ## set the cache matrix equal to the inverse of the matrix x
+
+        getinverse <- function() matrix ## return the cached inverse of x
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## inverse of the "matrix" created using following function
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        
+        matrix <- x$getinverse()
+        if(!is.null(matrix)) {
+                message("getting cached data")
+                return(matrix)
+        }
+        data <- x$get()
+        matrix <- solve(data, ...)
+        x$setinverse(matrix)
+        matrix
 }
